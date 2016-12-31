5b6de39615fc55b771ecdcc96f08201f9fe8a238
Alexey Khomych <alexeyfurious@gmail.com>
2016-12-31 16:33:00 +0200

    init project

---
+++ vkmusic/vkmusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vkmusic
+//
+//  Created by Alexey Khomych on 31.12.16.
+//  Copyright © 2016 Alexey Khomych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
